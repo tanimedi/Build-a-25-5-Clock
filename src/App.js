0669e7eed44d8140570189069e45f39c774e6635
Tanisha <tjmedi87@gmail.com>
2021-09-22 20:53:17 -0400

    set up and add elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [breaks, setBreaks] = React.useState(5);
+  const [session, setSession] = React.useState(25);
+  const [timerLabel, setTimerLabel] = React.useState("Session");
+  const [timer, setTimer] = React.useState("25:00");
+  const [onOff, setOnOff] = React.useState("off");
+
+  function reset() {
+    setBreaks(5);
+    setSession(25);
+  }
+  function breakDecrease() {
+    if (breaks === 1) {
+      setBreaks(breaks);
+    } else {
+      setBreaks(breaks - 1);
+    }
+  }
+  function breakIncrease() {
+    setBreaks(breaks + 1);
+  }
+  function sessionDecrease() {
+    setSession(session - 1);
+  }
+  function sessionIncrease() {
+    if (session === 60) {
+      setSession(session);
+    } else {
+      setSession(session + 1);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>25 + 5 Clock</div>
+      <div id="break-label">Break Length</div>
+      <div id="break-decrement" onClick={breakDecrease}>
+        down
+      </div>
+      <div id="break-length">{breaks}</div>
+      <div id="break-increment" onClick={breakIncrease}>
+        up
+      </div>
+
+      <div id="session-label">Session Length</div>
+      <div id="session-decrement" onClick={sessionDecrease}>
+        down
+      </div>
+      <div id="session-length">{session}</div>
+      <div id="session-increment" onClick={sessionIncrease}>
+        up
+      </div>
+
+      <div>
+        <div id="timer-label">{timerLabel}</div>
+        <div id="time-left">{timer}</div>
+      </div>
+
+      <div id="start_stop">start/stop</div>
+      <div id="reset" onClick={reset}>
+        reset
+      </div>
     </div>
   );
 }
-
-export default App;
